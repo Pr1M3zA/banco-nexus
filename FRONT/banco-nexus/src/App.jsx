d70f595c47da772bae6b30fba8e909fb7438eae1
Ernesto Gracia Nava <netonava012@gmail.com>
2026-05-09 15:17:35 -0700

    un poco mas dashboard

--- FRONT/banco-nexus/src/App.jsx
+++ FRONT/banco-nexus/src/App.jsx
@@ -1,15 +1,24 @@
 import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
 
 function App() {
+  //luego con el back cambair esto const usuario = await axios.get(...)
+  const usuario = {
+    nombre: "ernesto gracia",
+    tipoCuenta: "Cuenta Premium",
+  };
+
   return (
-    <div className="flex bg-gray-100">
+    <div className="flex bg-gray-100 min-h-screen">
+
       <Sidebar />
 
       <main className="flex-1 p-10">
-        <h1 className="text-4xl font-bold">
-          Dashboard Banco Nexus
-        </h1>
+
+        <Navbar usuario={usuario} />
+
       </main>
+
     </div>
   );
 }
